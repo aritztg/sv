39dbe923583f0bcb89394d868b4b093777f01d31
aritztg <aritztg@gmail.com>
2014-10-12 19:50:06 +0200

    Changed svc.py to svc and added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name="SVC",
+      version="0.1",
+      description="Spreadsheet Viewer and Converter",
+      author="Aritz Tusell",
+      author_email="aritztg at gmail",
+      url="https://github.com/aritztg/svc",
+      license="GPL",
+      scripts=["svc"]
+)
